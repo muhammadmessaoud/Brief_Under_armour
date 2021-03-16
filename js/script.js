3d08311a6add546d76b7c249928b62675d65cacb
Muhammad Messaoud <mumuh@MacBook-Pro-de-Muhammad.local>
2021-03-16 14:22:55 +0100

    Deuxieme jour

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let nouveaute = document.getElementById("nouveaute");
+let chaussures = document.getElementById("chaussures");
+let homme = document.getElementById("homme");
+let femme = document.getElementById("femme");
+let enfant = document.getElementById("enfant");
+let bonneAffaires = document.getElementById("bonnesaffaires") 
+let blue = document.getElementById("blue")
+
+
+
+nouveaute.addEventListener('mouseover',function (e) {
+    
+    blue.classList.add("red")
+})
+
+
+    
+
